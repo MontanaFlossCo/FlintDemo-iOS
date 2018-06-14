f5af27550b9c00735b732d7c3a42f3ba886701a8
Marc Palmer <marc@anyware.co.uk>
2018-06-14 09:53:17 +0100

    Updated to use Flint master new Activity API (#8)

--- FlintDemo/Features/Document Management/DocumentOpenAction.swift
+++ FlintDemo/Features/Document Management/DocumentOpenAction.swift
@@ -23,37 +23,40 @@ final class DocumentOpenAction: Action {
     
     static var analyticsID: String? = "document-open"
     
-    static var activityTypes: Set<ActivityEligibility> = [.perform, .handoff, .search]
-    
+#if canImport(Network)
+    static var activityTypes: Set<ActivityEligibility> = [.handoff, .prediction]
+#else
+    static var activityTypes: Set<ActivityEligibility> = [.handoff, .search]
+#endif
     static func perform(with context: ActionContext<DocumentRef>, using presenter: DocumentPresenter, completion: @escaping ((ActionPerformOutcome) -> ())) {
         presenter.openDocument(context.input)
         completion(.success(closeActionStack: false))
     }
     
-    static func prepare(activity: NSUserActivity, with input: InputType?) -> NSUserActivity? {
-        guard let documentRef = input else {
-            preconditionFailure("Input was expected")
+    static func prepareActivity(_ activity: ActivityBuilder<InputType>) {
+        guard let document = DocumentStore.shared.load(activity.input.name) else {
+            activity.cancel()
+            return
         }
-        
+
+        activity.title = activity.input.name
+        activity.subtitle = document.body
+        activity.thumbnail = UIImage(named: "FlintDemoDocIcon")
+
+        activity.userInfo["document"] = document.name
+
+        /// Hardcode some silly keywords for testing
+        activity.keywords = Set("decentralised internet patent".components(separatedBy: " "))
+
+        /// Search attributes
+
         // Note that this shouldn't really be here, inside an "Open" action.
         // Usually this would be for the case where you want to index something *and* make it available for
         // Siri suggestions, such as browsing document that the user didn't create themselves.
         // For the purposes of testing however, we leave it here. In reality you would use the CoreSpotlight APIs
         // directly to index all the known documents in the background at startup or similar.
 
-        guard let document = DocumentStore.shared.load(documentRef.name) else {
-            return nil
-        }
-
-        let searchAttributes: CSSearchableItemAttributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeText as String)
-        searchAttributes.addedDate = Date()
-        searchAttributes.kind = "Flint Demo Document"
-        searchAttributes.contentDescription = document.body
-        activity.contentAttributeSet = searchAttributes
-
-        /// Hardcode some silly keywords for testing
-        activity.keywords = Set("decentralised internet patent".components(separatedBy: " "))
-        activity.title = documentRef.name
-        return activity
+        activity.searchAttributes.addedDate = Date()
+        activity.searchAttributes.kind = "Flint Demo Document"
     }
 }
