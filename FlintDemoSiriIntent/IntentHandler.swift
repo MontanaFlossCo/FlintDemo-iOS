a404fd9632b3f8cf615910dae6ae69aef7381d7f
Marc Palmer <marc@anyware.co.uk>
2018-09-10 11:02:23 +0100

    Add an intent

---
+++ FlintDemoSiriIntent/IntentHandler.swift
@@ -0,0 +1,20 @@
+//
+//  IntentHandler.swift
+//  FlintDemoSiriIntent
+//
+//  Created by Marc Palmer on 10/09/2018.
+//  Copyright © 2018 Montana Floss Co. Ltd. All rights reserved.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+}
