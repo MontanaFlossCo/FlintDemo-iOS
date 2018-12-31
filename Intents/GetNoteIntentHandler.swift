030db9096ae30050857a633867ea8966bfd40abb
Marc Palmer <marc@anyware.co.uk>
2018-12-31 17:43:00 +0000

    Improve logging when intent fails to dispatch

--- Intents/GetNoteIntentHandler.swift
+++ Intents/GetNoteIntentHandler.swift
@@ -15,7 +15,10 @@ class GetNoteIntentHandler: NSObject, GetNoteIntentHandling {
     @objc(handleGetNote:completion:)
     func handle(intent: GetNoteIntent, completion: @escaping (GetNoteIntentResponse) -> Void) {
         let presenter = GetNoteSiriPresenter(completion: completion)
-        let _ = Flint.performIntentAction(intent: intent, presenter: presenter)
+        let outcome = Flint.performIntentAction(intent: intent, presenter: presenter)
+        if outcome != .success {
+            print("Intent failed: \(outcome)")
+        }
     }
     
 //    @objc(confirmGetNote:completion:)
