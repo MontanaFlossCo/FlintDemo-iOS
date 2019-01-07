7df060bdc0fd49709f0566b30b99d3ef3fd867b5
Marc Palmer <marc@anyware.co.uk>
2019-01-07 10:26:58 +0000

    Intent working for failure case, shared container not available yet for success case

--- FlintDemo/Features/Siri/GetNoteAction.swift
+++ FlintDemo/Features/Siri/GetNoteAction.swift
@@ -26,21 +26,30 @@ class GetNoteSiriPresenter: IntentResultPresenter {
     }
 }
 
+/// Take a Siri Intent for "Get Note" and try to load and present the result in Siri
 @available(iOS 12, *)
 final class GetNoteAction: IntentAction {
     typealias InputType = GetNoteIntent
     typealias PresenterType = GetNoteSiriPresenter
     
+    enum Failure: Error {
+        case documentNotFound
+    }
+    
     static func perform(context: ActionContext<InputType>, presenter: PresenterType, completion: Completion) -> Completion.Status {
         let response: GetNoteIntentResponse
+        let outcome: ActionPerformOutcome
         if let name = context.input.documentName, let document = DocumentStore.shared.load(name) {
             response = .success(content: document.body)
+            outcome = .successWithFeatureTermination
         } else {
+            // If they are to continue in-app we can pass an activity
             response = GetNoteIntentResponse(code: .failure, userActivity: nil)
+            outcome = .failureWithFeatureTermination(error: Failure.documentNotFound)
         }
         
         presenter.showResult(response: response)
-        return completion.completedSync(.successWithFeatureTermination)
+        return completion.completedSync(outcome)
     }
 }
 
