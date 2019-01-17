c392ccbf125336e99cb8719d72f2f64e8770b4ce
Marc Palmer <marc@anyware.co.uk>
2019-01-17 20:30:32 +0000

    Updated for new Intent APIs.

--- Intents/GetNoteIntentHandler.swift
+++ Intents/GetNoteIntentHandler.swift
@@ -10,15 +10,17 @@ import Foundation
 import Intents
 import FlintCore
 
+/// The intent handler for the GetNote intent.
+///
+/// We simply call in to Flint and pass the `intent` instance. Flint will dispatch the Action and
+/// pass the presenter to the action so that it can provide results to Siri, or instruct it to open the app.
 @objc
 class GetNoteIntentHandler: NSObject, GetNoteIntentHandling {
     @objc(handleGetNote:completion:)
     func handle(intent: GetNoteIntent, completion: @escaping (GetNoteIntentResponse) -> Void) {
         let presenter = GetNoteSiriPresenter(completion: completion)
         let outcome = Flint.performIntentAction(intent: intent, presenter: presenter)
-        if outcome != .success {
-            print("Intent failed: \(outcome)")
-        }
+        assert(outcome == .success, "Intent failed: \(outcome)")
     }
     
 //    @objc(confirmGetNote:completion:)
